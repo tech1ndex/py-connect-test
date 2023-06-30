f3242f85e3483969c2bb14a5a287ec9e478e240b
Victor Bajada <bajada.victor@gmail.com>
2023-06-30 12:21:13 -0400

    Moved to Function and Fixed Dockerfile

--- README.md
+++ README.md
@@ -3,16 +3,25 @@ Simple Python DockerFile to test connectivity to URLs and Log Status Code
 
 ### Usage
 
+List of `--urls` will need to be passed to Docker run/compose, the script will accept as many as required.
+
+usage: py-connect-test.py [-h] [-u [URLS ...]] [-l]
+
+options:
+
+  `-h`, `--help` - show this help message and exit
+  `-u [URLS ...]`, `--urls [URLS ...]` - A list of URLs to test against
+  `-l`, `--log` - Use --log if you want output logged to a file, default is stdout
+  `-p LOGPATH`, `--logpath LOGPATH` - Directory path to store logfile
+  `-i INTERVAL`, `--interval INTERVAL` - Interval at which to run the test in seconds, default value is 30
+
+### Docker
+
 - Provided DockerFile can be built as is and run
-
-### Parameters
-
-`-v` will need to be passed as the script expects to write a logfile to /log
-
- List of `urls` will need to be passed to Docker run/compose, the script will accept as many as required
+- `-v` will need to be passed if you plan to log to disk
 
  ##### Example:
 
 ```
- docker run -d -v /log:/log ghcr.io/tech1ndex/py-connect-test:latest "https://example.com" "https://example.com/test"
+ docker run -d -v /log:/log ghcr.io/tech1ndex/py-connect-test:latest --urls "https://example.com" "https://example.com/test"
  ```
---
+++ src/py-connect-test.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import requests
+from datetime import datetime
+import time
+import warnings
+import argparse
+
+# Parse Arguments
+parser = argparse.ArgumentParser(description='A simple HTTP connection tester written in Python.')
+parser.add_argument("-u", "--urls", help="A list of URLs to test against", nargs='*')
+parser.add_argument("-l", "--log", help="Use --log if you want output logged to a file, default is stdout", action="store_true")
+parser.add_argument("-p", "--logpath", help="Directory path to store logfile", default="--")
+parser.add_argument("-i", "--interval", help="Interval at which to run the test in seconds, default value is 30", type=int, default=30)
+args = parser.parse_args()
+
+# Logic to handle log parameters
+if args.log == True and args.logpath == "--":
+    parser.error('The --log argument requires the --logpath parameter to be defined')
+elif args.logpath != "--" and args.log == False:
+    parser.error('The --logpath argument requires the --log parameter to be provided')
+    exit
+
+# Suppress all warnings
+warnings.filterwarnings('ignore')
+
+# HTTP Function
+def httpTest():
+# Get Current Timestamp
+    now = datetime.now()
+    current_time = now.strftime("%m/%d/%Y - %H:%M:%S")
+# Get Status code from each URL passed in on the commandline    
+    try:
+        uptime_check=requests.get(u)
+        uptime_check.raise_for_status()
+        print(f"{current_time} - {u} - Success")
+    except requests.exceptions.RequestException as err:
+        # Write to file if connection times out to URL
+        uptime_status_code = str(uptime_check.status_code)
+        print(f"{current_time} - {u} - ERROR - Response: {uptime_status_code}")
+        # Write error to logfile if parameter was defined
+        if args.log == True:
+            logfile = open(f"{args.logpath}/py-connect-test.log", "a") #adding local path for testing purposes 
+            logfile.write(f"{current_time} - {u} - ERROR - Response: {uptime_status_code}")
+            logfile.write("\n")
+            logfile.close()
+
+# Main Block
+while True:
+    if len(args.urls) == 0:
+        print("No URL Received, please use -h to see list of parameters")
+        break
+    # Call httpTest func
+    for u in args.urls:
+      httpTest()
+                
+    #Sleep for time defined in Interval flag
+    time.sleep(args.interval)
